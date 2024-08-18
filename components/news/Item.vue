37f3a08d8dbd8dede1834b6cf09d25005575e82d
thatoranzhevyy <thatoranzhevyy@gmail.com>
2024-08-18 13:09:27 +0500

    изменения стилей профиля

--- components/news/Item.vue
+++ components/news/Item.vue
@@ -8,7 +8,7 @@ const readMoreActivated = ref(false);
 </script>
 
 <template>
-  <div class="bg-white dark:bg-gray-900 rounded-xl p-2">
+  <div class="bg-white dark:bg-gray-900 rounded-xl p-2 space-y-2">
     <nuxt-link :to="`id-${props.post_data.user.username}`" class="flex items-center" v-if="props.post_data.user">
       <u-avatar size="md" :alt="props.post_data.user.name"/>
       <div class="flex-1 min-w-0 ml-2">
@@ -16,13 +16,13 @@ const readMoreActivated = ref(false);
         <div class="text-gray-500 text-xs font-normal truncate">{{ dayjs(props.post_data.created_at).fromNow() }}</div>
       </div>
     </nuxt-link>
-    <div class="mt-2 text-sm">
+    <div class="text-sm">
       <span v-if="!readMoreActivated" class="select-text">{{ props.post_data.body.slice(0, 200) }}</span>
       <a v-if="!readMoreActivated" @click="readMoreActivated = !readMoreActivated"
          class="inline-block text-primary-500 px-1 rounded-md hover:bg-primary-200/20">Показать еще</a>
       <span v-if="readMoreActivated" class="select-text" v-html="props.post_data.body"></span>
     </div>
-    <div v-if="props.post_data.images" class="mt-2 flex flex-col">
+    <div v-if="props.post_data.images" class="flex flex-col">
       <div class="flex flex-row flex-wrap gap-0.5">
         <div v-for="image in props.post_data.images" class="relative border-box overflow-hidden flex flex-auto items-center size-32 sm:size-40 md:size-48 lg:size-52">
           <img class="rounded absolute w-full h-full left-0 right-0 top-0 bottom-0 object-cover"
--- pages/id-[username].vue
+++ pages/id-[username].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+definePageMeta({layout: 'guest'})
 const route = useRoute()
 const user = ref({})
 
@@ -21,16 +22,17 @@ onMounted(() => {
     <app-application-bar>
       <template #left>
         <u-button size="lg" color="gray" variant="ghost" icon="i-ph-arrow-left-bold" @click="$router.back()"/>
+        <div class="flex items-center">
+          <u-avatar :alt="user.name" size="md"/>
+          <div class="flex-1 min-w-0 ml-2">
+            <div class="text-sm font-semibold truncate">{{ user.name }}</div>
+            <div class="text-gray-500 text-xs font-normal truncate">{{ user.email }}</div>
+          </div>
+        </div>
       </template>
     </app-application-bar>
     <div>
-      <div class="flex items-center p-2">
-        <u-avatar :alt="user.name" size="md"/>
-        <div class="flex-1 min-w-0 ml-2">
-          <div class="text-sm font-semibold truncate">{{ user.name }}</div>
-          <div class="text-gray-500 text-xs font-normal truncate">{{ user.email }}</div>
-        </div>
-      </div>
+
     </div>
 
     <div class="max-w-3xl mx-auto py-2 space-y-2">
