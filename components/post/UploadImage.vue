ba5191d5249602f8aecd373fa12b9e8bbb9298b1
thatoranzhevyy <thatoranzhevyy@gmail.com>
2024-09-04 23:25:45 +0500

    добавленны формат фотографии

--- components/post/UploadImage.vue
+++ components/post/UploadImage.vue
@@ -54,7 +54,7 @@ const uploadImage = (files) => {
     </div>
     <UButton onclick="document.getElementById('fileInput').click()" color="gray" variant="solid"
              icon="i-ph-images" size="xs"/>
-    <UInput id="fileInput" type="file" accept="image/jpeg" class="hidden" @change="uploadImage" multiple/>
+    <UInput id="fileInput" type="file" accept="image/*" class="hidden" @change="uploadImage" multiple/>
   </div>
 </template>
 
